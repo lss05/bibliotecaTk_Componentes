80be61e10749048a85b3f94b6d2e8e9c77433e9e
Rafael De Leon Serafim <62965920+rafael-rfzorzi@users.noreply.github.com>
2020-10-16 19:03:53 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='tk_widgets_style_plus',
+    version='0.1.1',
+    url='https://github.com/rafael-rfzorzi/estiloWidgets',
+    license='MIT License',
+    author='Rafael De Leon Serafim',
+    author_email='rafaelserafim.rfzorzi@gmail.com',
+    keywords='autocomplety buttonStyle entryPlaceHolder gradienteFrame labelStyle',
+    description=u'Modulo que adiciona recursos extras ao tkinter',
+    packages=['estiloWidgets'],
+    install_requires=[],
+)
